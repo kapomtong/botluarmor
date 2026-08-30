5102cae35223fb1ebc8c0407500857076514a14a
kapomtong <kapomtong121@gmail.com>
2026-08-30 14:31:25 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ local response = req({
     Method = "POST",
     Headers = {
         ["Content-Type"] = "application/json",
-        ["x-client-secret"] = "YOUR_SHARED_SECRET" -- ใส่ CLIENT_SHARED_SECRET ให้ตรงกับใน .env
+        ["x-client-secret"] = "410011218fc0c121022833fd0527832fc67880712d4870179a85073531623ec9" -- ใส่ CLIENT_SHARED_SECRET ให้ตรงกับใน .env
     },
     Body = HttpService:JSONEncode({
         discord_id = userKey, -- ส่งค่า Key ไปในช่อง discord_id เพื่อให้ตรงกับที่ server.js รอรับ
